5b5b30d75ec6675e217655322e57636f1fa42ba2
Alexander Kudryk <alexander.kudryk@thestreet.com>
2018-03-12 00:08:08 -0400

    adding router, navigation, and form (need to set submit action)

---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+// import { Picker } from 'react-native';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
+import { Card, CardSection, Input, Button } from './common';
+
+class EmployeeCreate extends Component {
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeHolder="Jane"
+            value={this.props.name}
+            onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeHolder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
+          />
+        </CardSection>
+
+        <CardSection>
+          {/* <Picker
+            selectedValue={this.state.language}
+            onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
+            <Picker.Item label="Java" value="java" />
+            <Picker.Item label="JavaScript" value="js" />
+          </Picker> */}
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { 
+  employeeUpdate
+})(EmployeeCreate);
---
+++ src/components/EmployeeList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+// import PropTypes from 'prop-types'
+// import { connect } from 'react-redux'
+
+export default class EmployeeList extends Component {
+//   static propTypes = {
+//     prop: PropTypes
+//   }
+
+  render() {
+    return (
+      <View>
+        <Text> Employee List </Text>
+        <Text> Employee List </Text>
+        <Text> Employee List </Text>
+        <Text> Employee List </Text>
+        <Text> Employee List </Text>
+        <Text> Employee List </Text>
+      </View>
+    );
+  }
+}
+
+// const mapStateToProps = (state) => ({
+  
+// })
+
+// const mapDispatchToProps = {
+  
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(EmployeeList)
